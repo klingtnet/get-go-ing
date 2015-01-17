be699d34f4de803b19c423b6170eebd257916add
Andreas Linz <klingt.net@gmail.com>
2015-01-17 17:37:35 +0100

    functions

--- src/func.go
+++ src/func.go
@@ -6,8 +6,22 @@ import (
 
 func main() {
     fmt.Println(add(12, 30))
+    fmt.Println(swap("Linz", "Andreas"))
 }
 
-func add(a int, b int) int {
+func add(a, b int) int {
     return a + b
+}
+
+// return any number of arguments
+func swap(a, b string) (string, string) {
+    return b, a
+}
+
+// named return values
+// use this only in short functions
+func split(sum int) (x, y int) {
+    x = sum * 4 / 9
+    y = sum - x
+    return
 }
